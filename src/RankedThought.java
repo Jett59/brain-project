9953b05059c241db0a8c5459a2227e43474e997b
Jett Thompson <jett.thompson.59@gmail.com>
2019-03-04 18:00:01 +1100

    init commit

---
+++ src/RankedThought.java
@@ -0,0 +1,19 @@
+
+public class RankedThought {
+private double num;
+private thought thought;
+@Override
+public String toString() {
+	return thought.getS();
+}
+public double getNum() {
+return num;
+}
+public thought getThought() {
+return thought;
+}
+public RankedThought(double num, thought thought) {
+this.num = num;
+this.thought = thought;
+}
+}
